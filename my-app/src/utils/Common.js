eb69be09d7bbe82a1ae37893b09b92a31e9f9df7
Tanuja Gurung <67540467+TanujaGurung@users.noreply.github.com>
2021-10-18 12:53:25 +0530

    Update Common.js

--- my-app/src/utils/Common.js
+++ my-app/src/utils/Common.js
@@ -10,12 +10,27 @@ export  const getUser =() =>{
 export const getToken =() =>{
     return sessionStorage.getItem("token") || null;
 }
-export const setUserSession =(token, user) =>{
+export const setUserSession =(token, user,address,mobile,id,roles) =>{
     sessionStorage.setItem("token" ,token);
+    sessionStorage.setItem("id" ,id);
+    sessionStorage.setItem("mobile" ,mobile);
+    sessionStorage.setItem("address", JSON.stringify(address));
     sessionStorage.setItem("user", JSON.stringify(user));
+    sessionStorage.setItem("roles", JSON.stringify(roles));
 
 }
-
+export const getMobile =() =>{
+    return sessionStorage.getItem("mobile") || null;
+}
+export const getAddress =() =>{
+    return sessionStorage.getItem("address") || null;
+}
+export const getId =() =>{
+    return sessionStorage.getItem("id") || null;
+}
+export const getRoles =() =>{
+    return sessionStorage.getItem("roles") || null;
+}
 
 export const removeUserSession=() =>{
 sessionStorage.removeItem("token");
